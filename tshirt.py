7099a7e5c26ffda0881df1dafe0bb8dcc95b4ba5
floune <rfournier@simplonprod.co>
2021-08-22 14:38:17 +0200

    text to speech

--- tshirt.py
+++ tshirt.py
@@ -1,18 +1,22 @@
 import sys
+import os
 import speech_recognition as sr
 import pyaudio
 from deep_translator import GoogleTranslator
+from gtts import gTTS
+from io import BytesIO
+from playsound import playsound
 
 micro = sr.Microphone()
 fromlang = "fr"
 tolang = "en"
 r = sr.Recognizer()
+vocal = False
 
 languages = {
     "espagnol": "es",
     "allemand": "de",
     "portugais": "pt",
-    "chinois": "fz",
     "japonais": "ja",
     "arabe": "ar",
     "français": "fr",
@@ -24,7 +28,8 @@ languages = {
 def displayConfiguration():
     print("----Configuration----")
     print("langue de sortie: " + tolang)
-    print("-- Langues disponibles --")
+    print("mode vocal activé") if vocal else print("mode vocal désactivé")
+    print("--Langues disponibles--")
     for lang in languages:
         print(lang)
 
@@ -34,22 +39,42 @@ def configureTarget(newlang):
         tolang = languages[newlang]
         print("nouvelle langue de traduction: " + newlang)
 
-def megaTrans(text):
-    translated = GoogleTranslator(source=fromlang, target=tolang).translate(text=text)
+def megaTrans(whatDidYouSay):
+    translated = GoogleTranslator(source=fromlang, target=tolang).translate(text=whatDidYouSay)
     print(translated)
+    return translated
+
+
+def speak(wannaHear, info=False):
+    language = tolang if info == False else "fr"
+    tts = gTTS(text=wannaHear, lang=language)
+    filename = "tts.mp3"
+    tts.save(filename)
+    os.system("mpg123 " + filename)
+
+
+def recognize():
+    with micro as source:
+        audio_data = r.listen(source)
+        print("...")
+        return r.recognize_google(audio_data, language="fr-FR")
 
 
 def tshirt():
-
+    global vocal
     while True:
         try:
-            with micro as source:
-                audio_data = r.listen(source)
-                print("...")
-                result = r.recognize_google(audio_data, language="fr-FR")
-
+            result = recognize()
             if result == "configuration":
                 displayConfiguration()
+
+            elif result == "parle":
+                vocal = True
+                speak("d'accord", True)
+            
+            elif result == "ne parle plus":
+                vocal = False
+                speak("je me tais", True)
 
             elif result.split()[0] == "destination":
                 configureTarget(result.split()[1])
@@ -58,7 +83,9 @@ def tshirt():
                 sys.exit()
 
             else:
-                megaTrans(result)
+                res = megaTrans(result)
+                if vocal == True:
+                    speak(res)
         
         except sr.UnknownValueError:
             pass
